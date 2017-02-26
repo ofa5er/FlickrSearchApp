cf2e97b28fccf0d03bf39040221e98185d233d5c
Fakher Oueslati <oueslati.fakher@gmail.com>
2017-02-26 15:30:09 -0500

    feat: Creation of the search view
    - Created a menu containing a search view action item
    - Added persistence to search history to the search action item
    
    Resolves: #1

--- app/src/main/java/me/oueslati/fakher/flickrapp/MainActivity.java
+++ app/src/main/java/me/oueslati/fakher/flickrapp/MainActivity.java
@@ -1,13 +1,43 @@
 package me.oueslati.fakher.flickrapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+
+import me.oueslati.fakher.flickrapp.widget.AutoCompleteSearchView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private AutoCompleteSearchView mSearchView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialize();
+    }
+
+    private void initialize() {
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        mSearchView = (AutoCompleteSearchView) menu.findItem(R.id.action_search).getActionView();
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                mSearchView.saveKeywordsHistory(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        return true;
     }
 }
---
+++ app/src/main/java/me/oueslati/fakher/flickrapp/widget/AutoCompleteSearchView.java
@@ -0,0 +1,102 @@
+package me.oueslati.fakher.flickrapp.widget;
+
+/**
+ * Created by Fakher Oueslati on 2/26/2017.
+ */
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.widget.SearchView;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+
+import me.oueslati.fakher.flickrapp.R;
+
+/**
+ * Created by Fakher Oueslati on 2/26/2017.
+ */
+
+public class AutoCompleteSearchView extends SearchView {
+    private SearchView.SearchAutoComplete mSearchAutoComplete;
+
+    public AutoCompleteSearchView(Context context) {
+        super(context);
+        initialize();
+    }
+
+    public AutoCompleteSearchView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initialize();
+    }
+
+    public void initialize() {
+        mSearchAutoComplete = (SearchAutoComplete) findViewById(
+                android.support.v7.appcompat.R.id.search_src_text);
+
+        mSearchAutoComplete.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                mSearchAutoComplete.setText(adapterView.getItemAtPosition(i).toString());
+
+                //Place cursor at the end
+                mSearchAutoComplete.setSelection(
+                        adapterView.getItemAtPosition(i).toString().length());
+            }
+        });
+        loadSearchHistory();
+
+    }
+
+    private void loadSearchHistory() {
+        SharedPreferences settings = getContext().getSharedPreferences("keywords_history", 0);
+        int size = settings.getInt("history_size", 0);
+        if (size != 0) {
+            String[] history = new String[size];
+
+            for (int i = 0; i < size; i++) {
+                history[i] = settings.getString(String.valueOf(i + 1), "empty");
+            }
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(this.getContext(),
+                    R.layout.search_history_dropdown, history);
+            mSearchAutoComplete.setAdapter(adapter);
+        }
+    }
+
+    /**
+     * Save the keywords in the SharedPreferences keywords_history
+     *
+     * @param newKeyword the keyword to save in the search history
+     */
+    public void saveKeywordsHistory(String newKeyword) {
+        SharedPreferences settings = this.getContext().getSharedPreferences("keywords_history", 0);
+        int size = settings.getInt("history_size", 0);
+        String[] history = new String[size + 1];
+        boolean repeat = false;
+        for (int i = 0; i < size; i++) {
+            history[i] = settings.getString(String.valueOf(i + 1), "empty");
+            if (history[i].equals(newKeyword)) {
+                repeat = true;
+            }
+        }
+        if (!repeat) {
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putString(String.valueOf(size + 1), newKeyword);
+            editor.putInt("history_size", size + 1);
+            editor.apply();
+            history[size] = newKeyword;
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(this.getContext(),
+                    R.layout.search_history_dropdown, history);
+            mSearchAutoComplete.setAdapter(adapter);
+        }
+    }
+
+
+    public void setAdapter(ArrayAdapter<?> adapter) {
+        mSearchAutoComplete.setAdapter(adapter);
+    }
+
+
+}
